a6995a5f0b2d57d9e07d455713185befe4fee3de
Arttu Pennanen <39066923+pennane@users.noreply.github.com>
2024-04-19 13:49:45 +0300

    dont minify js

--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import arraybuffer from 'vite-plugin-arraybuffer'
 
 export default defineConfig({
-  plugins: [arraybuffer()]
+  plugins: [arraybuffer()],
+  build: {
+    minify: false
+  }
 })
